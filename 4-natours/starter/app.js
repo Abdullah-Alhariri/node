daaabf737ca47626f0bfe8b6dd16d4ebc596f2a3
Abdullah Alhriri <alhariri.abdu.2018@gmail.com>
2021-08-16 10:37:27 +0200

    post request added

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 
+app.use(express.json()) // To get the body-data from the use in post request (middleware). if you don't add this you will get undefined if you want to acces the req.body
+
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
 
 app.get('/api/v1/tours', (req,res)=>{
@@ -10,6 +12,21 @@ app.get('/api/v1/tours', (req,res)=>{
         "results": tours.length,
         "data": {tours},
     })    
+})
+
+app.post('/api/v1/tours', (req,res)=>{
+    const newId  = tours[tours.length -1].id + 1;
+    const newTour = Object.assign({id: newId},req.body ) // This overwrites original variable
+     tours.push(newTour)
+    
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours),(err)=>{ // Stringify : JS => JSON,   
+        res.status(201).json({ // 201 = created
+            "status":"success",
+            "results": tours.length,
+            "data": {tour : newTour},
+        })
+    })
+
 })
 
 const port = 3000
