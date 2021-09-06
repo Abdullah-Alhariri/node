762aaff9e4923c88566572c343df1fe676ee865e
Abdullah Alhriri <alhariri.abdu.2018@gmail.com>
2021-09-06 16:16:48 +0200

    middleWare added

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -5,6 +5,7 @@ const app = express();
 const tourRouter = require("./routes/tourRoutes.js");
 const userRouter = require("./routes/userRoutes.js");
 
+app.use(express.static(`${__dirname}/public`)); // normaly you cant acces files from browsers. this let files/people get acces to files from the URL
 app.use(morgan("dev"));
 app.use(express.json()); // To get the body-data from the use in post request (middleware). if you don't add this you will get undefined if you want to acces the req.body
 app.use("/api/v1/tours", tourRouter);
