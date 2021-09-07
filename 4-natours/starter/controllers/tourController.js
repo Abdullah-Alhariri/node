492e83fefee01a0c71c72197cc31dad7e3ae383c
Abdullah Alhriri <alhariri.abdu.2018@gmail.com>
2021-09-07 21:33:38 +0200

    creating tour added

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -17,12 +17,21 @@ exports.getTour = (req, res) => {
   //   },
   // });
 };
-exports.createTour = (req, res) => {
-  res.status(201).json({
-    status: "Success",
-    // results: tours.length,
-    // data: { tour: newTour },
-  });
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+
+    res.status(201).json({
+      status: "Success",
+      // results: tours.length,
+      data: { tour: newTour },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: "Invalid data set! (change this message)",
+    });
+  }
 };
 exports.updateTour = (req, res) => {
   res.status(204).json({
