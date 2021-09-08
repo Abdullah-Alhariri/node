8922545ee85d179459e941c3db5a6bdf507d577c
Abdullah Alhriri <alhariri.abdu.2018@gmail.com>
2021-09-08 06:51:19 +0200

    schema update

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -5,15 +5,52 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, "A tour must have a name"],
     unique: true,
+    trim: true,
+  },
+  duration: {
+    type: Number,
+    required: [true, "A tour must have a durations"],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, "A tour must have a group size"],
+  },
+  difficulty: {
+    type: String,
+    required: [true, "A tour must have a difficulty"],
   },
   price: {
     type: Number,
     required: [true, "A tour must have a price"],
   },
-  rating: {
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, "A tour must have a description"],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, "A tour must have a cover image"],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  ratingsAverange: {
     type: Number,
     default: 4.5,
   },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
+  },
+  startDates: [Date],
+  images: [String],
+  priceDiscount: Number,
 });
 const Tour = mongoose.model("Tour", tourSchema);
 
