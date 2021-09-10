ae7f1fc13f09d5e62e4b15ccc591e4bf47a0babb
Abdullah Alhriri <alhariri.abdu.2018@gmail.com>
2021-09-10 20:09:59 +0200

    production error handeling improvments

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -22,11 +22,32 @@ app.all("*", (req, res, next) => {
 app.use((err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
+  if (process.env.NODE_ENV === "development") {
+    res.status(err.statusCode).json({
+      status: err.status,
+      error: err,
+      message: err.message,
+      stack: err.stack,
+    });
+  } else if (process.env.NODE_ENV === "production") {
+    if (err.isOperational) {
+      res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+      });
+    } else {
+      let error = { ...err };
+      if (err.name === "CastError") {
+        const message = `Invalid ${error.path}: ${error.value}`;
+        error = new AppError(message, 400);
+      }
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+      res.status(500).json({
+        status: "Error",
+        message: error.message,
+      });
+    }
+  }
 });
 
 module.exports = app;
