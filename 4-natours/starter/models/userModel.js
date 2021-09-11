3f44d461204a87a6a0b0faf378ff7614833d18cd
Abdullah Alhriri <alhariri.abdu.2018@gmail.com>
2021-09-11 16:07:16 +0200

    password hashing added

--- 4-natours/starter/models/userModel.js
+++ 4-natours/starter/models/userModel.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
-// name, email, photo, password, passwordConfirm
+const bcrypt = require("bcryptjs");
+
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -22,8 +23,24 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, "Please confirm your password!"],
-    minlength: 8,
+    validate: {
+      // This only works on CREATE and SAVE!
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: "Passwords are not the same",
+    },
   },
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+
+  console.log(this.password);
+
+  next();
 });
 
 const User = mongoose.model("User", userSchema);
