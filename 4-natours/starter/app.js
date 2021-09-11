0540f11531d0408d892d4d4166cc8af6aa14ae22
Abdullah Alhriri <alhariri.abdu.2018@gmail.com>
2021-09-11 09:30:08 +0200

    handeling duble name tour error

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const AppError = require("./utils/appError");
 const app = express();
+const globalErrorHandler = require("./controllers/errorController");
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 
@@ -16,38 +17,9 @@ app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 
 app.all("*", (req, res, next) => {
-  next(new AppError(`Can't find f${req.originalUrl} on this server!`, 404));
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || "error";
-  if (process.env.NODE_ENV === "development") {
-    res.status(err.statusCode).json({
-      status: err.status,
-      error: err,
-      message: err.message,
-      stack: err.stack,
-    });
-  } else if (process.env.NODE_ENV === "production") {
-    if (err.isOperational) {
-      res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message,
-      });
-    } else {
-      let error = { ...err };
-      if (err.name === "CastError") {
-        const message = `Invalid ${error.path}: ${error.value}`;
-        error = new AppError(message, 400);
-      }
-
-      res.status(500).json({
-        status: "Error",
-        message: error.message,
-      });
-    }
-  }
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
