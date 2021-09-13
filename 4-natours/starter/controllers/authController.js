e29f65cdad845b7b1e78165f574633380db6b6e3
Abdullah Alhriri <alhariri.abdu.2018@gmail.com>
2021-09-13 07:44:14 +0200

    reset password functionality added

--- 4-natours/starter/controllers/authController.js
+++ 4-natours/starter/controllers/authController.js
@@ -1,3 +1,4 @@
+const crypto = require("crypto");
 const { promisify } = require("util");
 const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
@@ -111,4 +112,29 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   }
 });
 
-exports.resetPassword = catchAsync(async (req, res, next) => {});
+exports.resetPassword = catchAsync(async (req, res, next) => {
+  // 1) Get user based on the token
+  const hashedToken = crypto.createHash("sha256").update(req.params.token).digest("hex"); // Hashing the original random digits reset token for compairing
+  const user = await User.findOne({
+    passwordResetToken: hashedToken,
+    passwordResetExpires: { $gt: Date.now() },
+  });
+
+  // 2) If token has not expored, and there is user, set the new password
+  if (!user) next(new AppError("Token is invalid or has expired", 400));
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  user.passwordResetToken = undefined;
+  user.passwordResetExpires = undefined;
+  // .Save is used instead of findOneAndUpdate Because we want to run all the validators in the userModel
+  await user.save(); // For updating password, passwordConfirm, passwordResetToken and passwordResetExpires in the DB
+
+  // 3) Update changedPasswordAt property for the user
+
+  // 4) Log the user in, send JWT
+  const token = signToken(user._id);
+  res.status(200).json({
+    status: "success",
+    token,
+  });
+});
--- 4-natours/starter/routes/userRoutes.js
+++ 4-natours/starter/routes/userRoutes.js
@@ -9,7 +9,7 @@ router.post("/signup", authController.signup);
 router.post("/login", authController.login);
 
 router.post("/forgotPassword", authController.forgotPassword);
-router.patch("/resetPassword/:token", authController.signup);
+router.patch("/resetPassword/:token", authController.resetPassword);
 
 router.route("/").get(userController.getAllUsers).post(userController.createUsers);
 router
