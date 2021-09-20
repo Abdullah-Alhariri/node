39c84ad6179f27930dc64336f1edc34d768d286d
Abdullah Alhriri <alhariri.abdu.2018@gmail.com>
2021-09-20 18:19:03 +0200

    merging public js in one file

--- 4-natours/public/js/mapbox.js
+++ 4-natours/public/js/mapbox.js
@@ -1,46 +1,45 @@
 /* eslint-disable */
-console.log('hello from the client side');
 
-const locations = JSON.parse(document.getElementById('map').dataset.locations);
+export const displayMap = locations => {
+  mapboxgl.accessToken =
+    'pk.eyJ1IjoiYWJkdWxsYWgtYWxoYXJpcmkiLCJhIjoiY2t0c2s5c3o2MDdtZzJwb2RzYXVrNWh2OSJ9.lkR3rQJ5w8_-HHEpQTL75w';
 
-mapboxgl.accessToken =
-  'pk.eyJ1IjoiYWJkdWxsYWgtYWxoYXJpcmkiLCJhIjoiY2t0c2s5c3o2MDdtZzJwb2RzYXVrNWh2OSJ9.lkR3rQJ5w8_-HHEpQTL75w';
+  const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/abdullah-alhariri/cktskd8zx57yu18pbi6frlo7a', //light mode
+    //   style: 'mapbox://styles/abdullah-alhariri/cktso9ewk01ep17p7qlt3qqzo',// dark mode
+    scrollZoom: false
+  });
+  const bounds = new mapboxgl.LngLatBounds(); // 'mapboxgl.LatLngBounds' comes from block append head scripts
 
-const map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/abdullah-alhariri/cktskd8zx57yu18pbi6frlo7a', //light mode
-  //   style: 'mapbox://styles/abdullah-alhariri/cktso9ewk01ep17p7qlt3qqzo',// dark mode
-  scrollZoom: false
-});
-const bounds = new mapboxgl.LngLatBounds(); // 'mapboxgl.LatLngBounds' comes from block append head scripts
+  locations.forEach(loc => {
+    // create marker
+    const el = document.createElement('div');
+    el.className = 'marker';
+    // add marker
+    new mapboxgl.Marker({
+      element: el,
+      anchor: 'bottom'
+    })
+      .setLngLat(loc.coordinates)
+      .addTo(map);
+    // add popup
+    new mapboxgl.Popup({
+      offset: 30
+    })
+      .setLngLat(loc.coordinates)
+      .setHTML(`<p>Day ${loc.day}: ${loc.description} </p>`)
+      .addTo(map);
+    // extend map bounds to include current location
+    bounds.extend(loc.coordinates);
+  });
 
-locations.forEach(loc => {
-  // create marker
-  const el = document.createElement('div');
-  el.className = 'marker';
-  // add marker
-  new mapboxgl.Marker({
-    element: el,
-    anchor: 'bottom'
-  })
-    .setLngLat(loc.coordinates)
-    .addTo(map);
-  // add popup
-  new mapboxgl.Popup({
-    offset: 30
-  })
-    .setLngLat(loc.coordinates)
-    .setHTML(`<p>Day ${loc.day}: ${loc.description} </p>`)
-    .addTo(map);
-  // extend map bounds to include current location
-  bounds.extend(loc.coordinates);
-});
-
-map.fitBounds(bounds, {
-  padding: {
-    top: 200,
-    bottom: 250,
-    left: 100,
-    right: 100
-  }
-});
+  map.fitBounds(bounds, {
+    padding: {
+      top: 200,
+      bottom: 250,
+      left: 100,
+      right: 100
+    }
+  });
+};
