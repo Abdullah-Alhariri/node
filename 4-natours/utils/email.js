8e9d465493245ef5a8106fe9edb9f99b3b128dc5
Abdullah Alhriri <alhariri.abdu.2018@gmail.com>
2021-09-24 13:38:36 +0200

    email handler update

--- 4-natours/utils/email.js
+++ 4-natours/utils/email.js
@@ -1,27 +1,52 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
 
-const sendEmail = async options => {
-  // 1) Create a transporter
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = `Abdullah Alhariri < ${process.env.EMAIL_FROM} >`;
+  }
+
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      // Sendgrid
+      return 1;
     }
-  });
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD
+      }
+    });
+  }
 
-  // 2) Define the email options
-  const mailOptions = {
-    from: 'Jonas Schmedtmann <hello@jonas.io>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message
-    // html:
-  };
+  async send(template, subject) {
+    // 1) Render the HTML based on a pug template
+    const html = pug.renderFile(
+      `${__dirname}/../views/emails/${template}.pug`,
+      { firstName: this.firstName, url: this.url, subject }
+    );
 
-  // 3) Actually send the email
-  await transporter.sendMail(mailOptions);
+    // 2) Define email options
+    const mailOptions = {
+      text: htmlToText.fromString(html),
+      from: this.from,
+      to: this.email,
+      subject,
+      html
+    };
+
+    // 3) Create a transport and send email
+
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send('welcome', 'welcome to the natours family!');
+  }
 };
-
-module.exports = sendEmail;
