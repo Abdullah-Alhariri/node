adbb684826fe0b75b2c5440aad58a98fe743a0d8
Abdullah Alhriri <alhariri.abdu.2018@gmail.com>
2021-09-24 21:04:02 +0200

    console.log remove

--- 4-natours/controllers/bookingController.js
+++ 4-natours/controllers/bookingController.js
@@ -41,7 +41,6 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 exports.createBookingCheckout = catchAsync(async (req, res, next) => {
   // This is only TEMPORARY, because it's unsecure: everyone can make bookings without paying
   const { tour, user, price } = req.query;
-  console.log({ tour, user, price });
   if (!tour && !user && !price) return next();
   await Booking.create({ tour, user, price });
   res.redirect(req.originalUrl.split('?')[0]); // this makes a new request to the same url (with no queries, so it will exit and by line 43)
